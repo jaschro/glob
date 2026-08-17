3b0ca42b82482df15b247dc62bdde419e2f46086
jaschro <jasonchroman@gmail.com>
2026-08-16 22:09:45 -0400

    Update Glob

--- push.bat
+++ push.bat
@@ -32,7 +32,7 @@ echo Building site to check for errors...
 hugo --minify >nul
 if errorlevel 1 goto :build_failed
 echo Build OK.
-goto :do_push
+goto :do_commit
 
 :build_failed
 echo.
@@ -42,7 +42,7 @@ goto :end
 :skip_hugo_build
 echo Hugo not found, skipping build check. Install from gohugo.io if you want this check to run.
 
-:do_push
+:do_commit
 git add -A
 
 set "MSG="
@@ -50,19 +50,39 @@ set /p MSG="Commit message, press Enter for 'Update Glob': "
 if "%MSG%"=="" set "MSG=Update Glob"
 
 git commit -m "%MSG%"
-if errorlevel 1 (
-    echo.
-    echo Nothing to commit, or the commit failed -- see above.
-    goto :end
-)
+if errorlevel 1 goto :commit_skipped
+goto :do_pull
 
+:commit_skipped
+echo.
+echo Nothing new to commit here.
+goto :do_pull
+
+:do_pull
+echo.
+echo Checking GitHub for anything saved from the Add or Edit page in the browser...
+git pull --no-rebase --no-edit
+if errorlevel 1 goto :pull_failed
+goto :do_push
+
+:pull_failed
+echo.
+echo Couldn't automatically merge in the latest changes from GitHub -- see the error above.
+echo This usually means the same post was changed both here and in the browser.
+echo Open the file it mentions, fix any conflict markers, save it, then run this again.
+goto :end
+
+:do_push
 git push
-if errorlevel 1 (
-    echo.
-    echo Push failed -- see the error above.
-    goto :end
-)
+if errorlevel 1 goto :push_failed
+goto :push_ok
 
+:push_failed
+echo.
+echo Push failed -- see the error above.
+goto :end
+
+:push_ok
 echo.
 echo Pushed. Check the Actions tab on GitHub to watch the build,
 echo then reload the site in a minute or two.
